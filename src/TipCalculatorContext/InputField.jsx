64323f52a72ec4d37c48896452f5c9f7004a1a8b
SarahHanks04 <sarahhanks04@gmail.com>
2024-09-05 23:37:08 +0100

    updated

--- src/TipCalculatorContext/InputField.jsx
+++ src/TipCalculatorContext/InputField.jsx
@@ -1,17 +1,18 @@
 import React, { useContext } from 'react';
 import { CalculatorContext } from './CalculatorContext';
+import "./Styles/InputField.css"
 
 const InputField = () => {
   const { bill, setBill } = useContext(CalculatorContext);
 
   return (
-    <div className="mb-4 text-left">
+    <div className="input mb-4 text-left">
       <label htmlFor="bill" className="text-white font-semibold mb-2">Bill</label>
       <input
         type="number"
         value={bill}
         placeholder="$"
-        className="w-full px-3 py-2 rounded-lg focus:outline-none focus:ring"
+        className="w-full px-3 rounded-lg"
         onInput={(e) => setBill(Number(e.target.value))}
       />
     </div>
--- src/TipCalculatorContext/MainCalculator.jsx
+++ src/TipCalculatorContext/MainCalculator.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Select from './Select';
 import InputField from './InputField';
 import NumberField from './NumberField';
+import "./Styles/main.css"
 
 const MainCalculator = () => {
   return (
-    <div className="bg-gray-900 rounded-lg shadow-lg max-w-[400px] mx-auto p-8">
+    <div className="main max-w-[400px] mx-auto p-8">
       <InputField />
       <Select />
       <NumberField />
